cec292a7038855ab8bc3a8d007deb78516e734fc
GustavoManzoni <gustavomanzoni2008@gmail.com>
2024-05-02 11:32:12 -0300

    Implemento da movimentação do player

---
+++ GitHub1/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+
+
+    private  Rigidbody body;
+    public float horizontal, speed = 15, vertical;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        body = GetComponent<Rigidbody>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontal = Input.GetAxisRaw("Horizontal");
+        body.velocity = new Vector3( 0, 0, speed * horizontal);
+        vertical = Input.GetAxisRaw("Vertical");
+        body.velocity = new Vector3(speed * vertical, 0, 0 );
+
+
+
+    }
+}
